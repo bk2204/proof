3e7296cea195c516ae22308fdc703e8222539e4a
brian m. carlson <sandals@crustytoothpaste.net>
2017-01-04 02:50:42 +0000

    spec: ensure we recognize SHA-3
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -22,6 +22,14 @@ describe ProofSig::Data::Algorithm do
     end
   end
 
+  it 'should recognize extended SHA-3 algorithms' do
+    %w(SHA3-512 SHA3-384 SHA3-256 SHA3-224 SHAKE-128 SHAKE-256).each do |name|
+      a = ProofSig::Data::Algorithm.new name
+      expect(a.to_s).to eq name
+      expect(a.secure?).to be true
+    end
+  end
+
   it 'should recognize common but insecure algorithms' do
     %w(MD2 MD4 MD5 SHA-0 SHA-1).each do |name|
       a = ProofSig::Data::Algorithm.new name
